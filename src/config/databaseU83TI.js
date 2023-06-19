c0b5cce872d5796328f872a5b54582954a889c2e
João Henrique De Oliveira Molina <107960091+Joaomolina85@users.noreply.github.com>
2023-06-19 09:15:11 -0300

    fix: passando novo host conexao com banco de dados

--- src/config/databaseU83TI.js
+++ src/config/databaseU83TI.js
@@ -1,6 +1,7 @@
 module.exports = {
   dialect: 'mysql',
-  host: 'sql536.main-hosting.eu',
+  // host: 'sql536.main-hosting.eu',
+  host: 'srv951.hstgr.io',
   username: 'u838556479_ti',
   password: '@7847%Twty',
   database: 'u838556479_ti',
