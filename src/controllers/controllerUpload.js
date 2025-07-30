d8b0407d6c2f65244414ae2dceb46ddccb27160a
Gerson Fagundes <54805604+fagundesoff@users.noreply.github.com>
2025-07-30 16:54:39 -0300

    teste de envio

--- src/controllers/controllerUpload.js
+++ src/controllers/controllerUpload.js
@@ -1,53 +1,48 @@
-const stream = require('stream');
-const path = require('path');
-const { google } = require('googleapis');
-const shortid = require('shortid');
-
-const KEYFILEPATH = path.join(__dirname, '../config/credentials.json');
-const SCOPES = ['https://www.googleapis.com/auth/drive'];
-
-const auth = new google.auth.GoogleAuth({
-    keyFile: KEYFILEPATH,
-    scopes: SCOPES
-})
-
-const uploadFile = async (body, fileObject) => {
-    const bufferStream = new stream.PassThrough();
-    bufferStream.end(fileObject.buffer);
-    const { data } = await google.drive({ version: 'v3', auth: auth }).files.create({
-      media: {
-        mimeType: fileObject.mimeType,
-        body: bufferStream,
-      },
-      requestBody: {
-        name: body.orcamento + '-' + shortid.generate() + '.' + fileObject.originalname.split('.')[1],
-        parents: ['1XeWF5eidcOqwl6skxp0AwBI2nE2cZvc_'],
-      },
-      fields: 'id,name',
-    });
-    console.log(`Uploaded file ${data.name} ${data.id}`);
-    return data.id;
-};
+const axios = require('axios');
+const FormData = require('form-data');
 
 const uploads = {
     async uploadFile(req, res) {
         try {
-            const {body, files} = req;
-            let file = null;
+            const { body, files } = req;
 
-            for (let f = 0; f < files.length; f += 1) {
-                file = await uploadFile(body, files[f]);
+            let responses = [];
+
+            for (let f = 0; f < files.length; f++) {
+                const file = files[f];
+
+                // Cria um FormData para enviar o arquivo e os dados
+                const form = new FormData();
+                form.append('file', file.buffer, {
+                    filename: file.originalname,
+                    contentType: file.mimetype,
+                });
+
+                // Adiciona dados extras do body ao form, se necessário
+                form.append('orcamento', body.orcamento || '');
+
+                // Envia para o webhook
+                const response = await axios.post(
+                    'https://n8n-labsystem-u40703.vm.elestio.app/webhook/portal',
+                    form,
+                    {
+                        headers: form.getHeaders(),
+                    }
+                );
+
+                responses.push(response.data);
             }
-            const link = `https://drive.google.com/uc?id=${file}&export=download`;
-            // https://drive.google.com/file/d/${file}/view?usp=sharing&export=download
 
-            res.status(200).json({msg: 'Upload realizado com sucesso!', link})
+            res.status(200).json({
+                msg: 'Arquivo(s) enviado(s) com sucesso ao webhook!',
+                results: responses,
+            });
+
         } catch (error) {
-            console.log(error)
-            res.status(400).json({msg: 'Erro ao realizar upload!'})
-            
+            console.error('Erro ao enviar para o webhook:', error);
+            res.status(400).json({ msg: 'Erro ao enviar para o webhook!' });
         }
     }
-}
+};
 
 module.exports = uploads;
