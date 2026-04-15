fafdfef491932dd95a5a77a5891c3973e36b171c
Gerson Fagundes <54805604+fagundesoff@users.noreply.github.com>
2026-04-15 18:18:40 -0300

    Revert "Correções para subir no s3"
    This reverts commit 2b82a60a121c1e946054449587368dcc4dd29f8a.

--- src/routes/midlleware/uploadRelatorio.js
+++ src/routes/midlleware/uploadRelatorio.js
@@ -1,12 +1,39 @@
+const stream = require('stream');
 const express = require('express');
 const multer = require('multer');
-const Router = express.Router();
-const controllerUpload = require('../controllers/controllerUpload');
+const { google } = require('googleapis');
 
-// Multer em memória (sem salvar em disco)
-const upload = multer({ storage: multer.memoryStorage() });
+const uploadRouter = express.Router();
+const upload = multer();
 
-Router.post('/uploadFile', upload.any(), controllerUpload.uploadFile);
-Router.get('/refreshUrl/:id', controllerUpload.refreshUrl);
+const uploadFile = async (fileObject) => {
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end(fileObject.buffer);
+    const { data } = await google.drive({ version: 'v3' }).files.create({
+      media: {
+        mimeType: fileObject.mimeType,
+        body: bufferStream,
+      },
+      requestBody: {
+        name: fileObject.originalname,
+        parents: ['DRIVE_FOLDER_ID'],
+      },
+      fields: 'id,name',
+    });
+    console.log(`Uploaded file ${data.name} ${data.id}`);
+};
 
-module.exports = Router;
+uploadRouter.post('/uploadFile', upload.any(), async (req, res) => {
+    try {
+        const {body, files} = req;
+
+        for (let f = 0; f < files.length; f += 1) {
+            await uploadFile(files[f]);
+        }
+
+        res.status(200).json({msg: 'Upload realizado com sucesso!'})
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({msg: 'Erro ao realizar upload!'})
+    }
+});
